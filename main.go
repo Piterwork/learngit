4a725e8ca23a7e5cafef1edd45d293da42dc1c70
Piterwork <piterwork3@gmail.com>
2026-05-22 18:52:48 -0300

    Esse é o dapasta original

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 func main() {
 	fmt.Println("Quero que fique assim")
 	fmt.Println("pronto")
+	fmt.Println("esse é do pasta1/original")
 	fmt.Println("Hi God")
 	fmt.Println("Blessed")
 	fmt.Println("Jesus é o caminho a verdade e vida")
